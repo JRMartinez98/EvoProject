80305424ab2568185064c75e8c42cda33d9a7d32
Jose <jsmartinez257@gmail.com>
2018-05-05 14:59:49 -0400

    update Population

--- src/Population.java
+++ src/Population.java
@@ -47,7 +47,7 @@ public class Population {
 		//convert parents to String
 		String p1 = parent1.toString();
 		String p2 = parent2.toString();
-
+		
 		//Permutation representation 
 		//1. choose 2 crossover points randomly
 		int crossPoint1 =(int) (Math.random()*10-1);
@@ -57,9 +57,11 @@ public class Population {
 		String child1 = p1.substring(0, min) + p2.substring(min, max) + p1.substring(max);
 		String child2 = p2.substring(0, min) + p1.substring(min, max) + p2.substring(max);
 		
-		//for (int i=0; i < p1.length();i++){
-			//start at first index point
-			// loop to see if that value has been copied over yet
+		StringBuffer child1SB = new StringBuffer(p1);
+		StringBuffer child2SB = new StringBuffer(p2);
+		
+
+		// loop to see if that value has been copied over yet
 		boolean isCopied =false;
 			while (min<max){
 			int value = p2.charAt(min);
@@ -73,17 +75,27 @@ public class Population {
 				//do nothing
 			}
 			else{ //else the value of the index in P2 is not copied over yet
-				//see what that position is being taken by in the offsring 
-				
+				//get value of index in P1
+				int indexValue = p1.charAt(min);
+				//search through P2 for position of that value
+				for (int i=0; i < p2.length();i++){
+					if (p2.charAt(i) == indexValue){
+						child1SB.insert(i, value);
+						
+					}
+				}
+	
 			}
 			
 			min++;	
 			}
-			
-		//}
+
 		
 	}
 
 	// 3. Mutation
+	public void mutation(StringBuffer child1, StringBuffer child2){
+		
+	}
 
 }
